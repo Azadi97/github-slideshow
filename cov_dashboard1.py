1d8f64433de876243280e79327fbecb674edb6e9
Azadi97 <gworld97@gmail.com>
2022-04-16 15:34:00 +0200

    'updated'

--- cov_dashboard1.py
+++ cov_dashboard1.py
@@ -8,4 +8,34 @@ df = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/pu
 cases = df[df['location'] == 'Afghanistan']['new_cases']
 dates = df[df['location'] == 'Afghanistan']['date']
 
+# Create a figure
+fig = plt.figure()
 plt.plot(dates, cases)
+
+# Plots the chart
+st.plotly_chart(fig)
+
+#----------------------------------------------
+# # MultySelector goes here
+# # Get the countries list
+clist = df['location'].unique()
+
+# Create the streamlit sidebar
+# country = st.sidebar.selectbox("Select a country:",clist)
+country = st.sidebar.multiselect("Select countries:",clist)
+
+# The header of the figure
+st.header("COVID-19 cases per country - Multy Countries")
+
+cases = df[df['location'] == country]['new_cases']
+dates = list(df[df['location'] == country]['date'])
+
+# Create a figure
+fig = plt.figure()
+
+# plt.figure(figsize=(12,5))
+plt.plot(dates, cases)
+
+# Plots the chart
+st.plotly_chart(fig)
+
